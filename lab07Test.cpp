4455fab199ae55dcc882cba5ee1efb72aee12dbc
Yuan Chun CHENG <yuanchun_cheng@ucsb.edu>
2025-03-08 15:42:54 -0800

    Update lab07Test.cpp

--- lab07Test.cpp
+++ lab07Test.cpp
@@ -1,16 +1,17 @@
 #include "WordCount.h"
-#include "tddFuncs.h" 
+#include "tddFuncs.h"
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
-void testWordCount() {
+int main() {
     WordCount wc;
 
     ASSERT_EQUALS("isn't", wc.makeValidWord("isn't"));
     ASSERT_EQUALS("mother-in-law", wc.makeValidWord("mother-in-law"));
     ASSERT_EQUALS("apple", wc.makeValidWord("...apple***"));
+    ASSERT_EQUALS("foo-bar", wc.makeValidWord("foo-bar-"));
     ASSERT_EQUALS("", wc.makeValidWord("-$%#"));
 
     wc.incrWordCount("foo");
@@ -18,7 +19,7 @@ void testWordCount() {
     wc.incrWordCount("bar");
     wc.incrWordCount("BAR"); 
     wc.incrWordCount("foo-bar");
-
+    
     ASSERT_EQUALS(2, wc.getWordCount("foo"));
     ASSERT_EQUALS(2, wc.getWordCount("bar"));
     ASSERT_EQUALS(1, wc.getWordCount("foo-bar"));
@@ -31,20 +32,21 @@ void testWordCount() {
     ASSERT_EQUALS(0, wc.getWordCount("foo"));
 
     wc.addAllWords("apple banana Apple cherry! banana, apple.");
-    ostringstream out1, out2;
+    ASSERT_EQUALS(3, wc.getWordCount("apple"));
+    ASSERT_EQUALS(2, wc.getWordCount("banana"));
+    ASSERT_EQUALS(1, wc.getWordCount("cherry"));
+
+    ostringstream out1;
     wc.dumpWordsSortedByWord(out1);
-    wc.dumpWordsSortedByOccurence(out2);
-
     string expectedSortedByWord = "apple,3\nbanana,2\ncherry,1\n";
-    string expectedSortedByOccurence = "apple,3\nbanana,2\ncherry,1\n";
-
     ASSERT_EQUALS(expectedSortedByWord, out1.str());
+
+    ostringstream out2;
+    wc.dumpWordsSortedByOccurence(out2);
+    string expectedSortedByOccurence = "apple,3\nbanana,2\ncherry,1\n";
     ASSERT_EQUALS(expectedSortedByOccurence, out2.str());
 
     cout << "All tests passed!" << endl;
-}
-
-int main() {
-    testWordCount();
     return 0;
 }
+
